e94249678e9e8e7b8b5f2f7f2257081977b5b8e6
IsabellaMacedo <isabellamacedocontato@gmail.com>
2021-01-22 15:55:12 -0300

    Updates

---
+++ precache-manifest.7791e7e6658a5bb3e39bae19af31440d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbebdc6dc03063c31859acd04cefb9aa",
+    "url": "/desafio/index.html"
+  },
+  {
+    "revision": "9699693f80b856d0c2a9",
+    "url": "/desafio/static/css/main.a712c2a8.chunk.css"
+  },
+  {
+    "revision": "4648b0da999c0325f8de",
+    "url": "/desafio/static/js/2.2eec3150.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/desafio/static/js/2.2eec3150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9699693f80b856d0c2a9",
+    "url": "/desafio/static/js/main.6b9e30d9.chunk.js"
+  },
+  {
+    "revision": "3ee4bbfa739ad11564a6",
+    "url": "/desafio/static/js/runtime-main.df98c35a.js"
+  },
+  {
+    "revision": "90cff973d30a6cdbba5445bbce399dd2",
+    "url": "/desafio/static/media/login-img.90cff973.svg"
+  }
+]);
